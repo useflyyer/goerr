7703609eb33cdf6ebc573bffa6c97845c7513bb9
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-02-13 10:16:21 -0300

    chore: Change order of error and value

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export type Result<T, E extends Error> = [T, E | null];
 
-export default function goerr<T, E extends Error>(func: () => T): Result<T, E>;
-export default function goerr<T, E extends Error>(promise: PromiseLike<T>): Promise<Result<T, E>>;
-export default function goerr<T, E extends Error>(asyncFunc: () => Promise<T>): Promise<Result<T, E>>;
+export function goerr<T, E extends Error>(func: () => T): Result<T, E>;
+export function goerr<T, E extends Error>(promise: PromiseLike<T>): Promise<Result<T, E>>;
+export function goerr<T, E extends Error>(asyncFunc: () => Promise<T>): Promise<Result<T, E>>;
 
-export default function goerr(parameter: any): any {
+export function goerr(parameter: any): any {
   if ("then" in parameter && "catch" in parameter) {
     return parameter.then((r: any) => [r, null]).catch((err: Error) => [null, err]);
   }
